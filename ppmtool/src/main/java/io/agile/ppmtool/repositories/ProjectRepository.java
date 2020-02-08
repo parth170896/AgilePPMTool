4fec0df6fef46560440045ccfed093c2b08ee1e9
Parth Rathod <parthrathod170896@gmail.com>
2020-02-08 15:35:11 +0530

    Repository Implementation

---
+++ ppmtool/src/main/java/io/agile/ppmtool/repositories/ProjectRepository.java
@@ -0,0 +1,13 @@
+package io.agile.ppmtool.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import io.agile.ppmtool.domain.Project;
+
+@Repository
+public interface ProjectRepository extends CrudRepository<Project, Long>{
+
+	@Override
+	Iterable<Project> findAllById(Iterable<Long> iterable);
+}
